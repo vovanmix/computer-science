ee70f7c38efb5f4d44fd16fa548bd6875040970a
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-11 23:26:05 -0700

    Update Stacks.php

--- DataStructures/Stacks.php
+++ DataStructures/Stacks.php
@@ -1,7 +1,8 @@
 <?php
 
-# First In Last Out structure
+# Last In First Out (LIFO) structure
 # 3 methods: push, pop, peek
+# Could be implemented depending of a language natively, with a list or array behind the curtain
 
 $a = new SplStack();
 $a->push('1');
