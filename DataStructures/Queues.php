445fc11adb906b2c7060d45771ad1fd161f18ae5
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-11 23:35:08 -0700

    Update Queues.php

--- DataStructures/Queues.php
+++ DataStructures/Queues.php
@@ -2,3 +2,14 @@
 
 # First in, first out (FIFO)
 # Abstract data type
+# methods put, shift
+
+$q = new SplQueue();
+$q->enqueue(1);
+$q->enqueue(2);
+$q->enqueue(3);
+
+while($q->count()){
+  $x = $q->dequeue();
+  echo "{$x}";
+}
