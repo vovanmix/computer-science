1daf70e57934099ca097a8616006482dc2a38819
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-11 20:08:11 -0700

    Create linkedLists.php

---
+++ DataStructures/linkedLists.php
@@ -0,0 +1,6 @@
+<?php
+
+# Linked list is a group of elements, each of them have a link (poiner, reference) to the next (linked list) 
+# and previous (double linked list) element. Elements can not be accessed by index. Elemets are accessed only in consequence.
+# Lists are better than arrays in adding elements (faster in memory). But is lists are implemented using arrays there is no difference.
+# Linear Search complexity: O(n) (arrays are better( O(log n)) in binary search)
