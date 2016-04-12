8b4d925bc5de2aea4da241a64359e65f8fb19b12
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-11 21:36:54 -0700

    Create Stacks.php

---
+++ DataStructures/Stacks.php
@@ -0,0 +1,13 @@
+<?php
+
+# First In Last Out structure
+# 3 methods: push, pop, peek
+
+$a = new SplStack();
+$a->push('1');
+$a->push('2');
+$a->push('3');
+
+while($x = $a->pop()){
+  echo "{$x} "
+}
