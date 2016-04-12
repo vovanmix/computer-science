86a8862936a805d9a69939fc9a26d63095046dbc
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-11 23:27:30 -0700

    Update Stacks.php

--- DataStructures/Stacks.php
+++ DataStructures/Stacks.php
@@ -1,6 +1,7 @@
 <?php
 
 # Last In First Out (LIFO) structure
+# Abstract data type
 # 3 methods: push, pop, peek
 # Could be implemented depending of a language natively, with a list or array behind the curtain
 
