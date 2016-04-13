6f1032a6ce8ebb367a78ea94315c701439c857bf
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-13 00:04:34 -0700

    Update BinarySearch.php

--- Algorythms/BinarySearch.php
+++ Algorythms/BinarySearch.php
@@ -5,29 +5,25 @@
 # Binary Search: O(log(n))
 # Linear (Brute Force):	O(n)
 
-
-
 function binary_search($needle, $haystack){
-
     $left = 0;
     $right = count($haystack) - 1;
     
     while($left <= $right){
-        $mid = ($left + $right) / 2;
+        $mid = round(($left + $right) / 2);
         
         if($haystack[$mid] == $needle){
             return $mid;
         } elseif ($haystack[$mid] > $needle){
             $right = $mid - 1;
-        } elseif($list[$mid] < $x) {
+        } elseif($haystack[$mid] < $needle) {
             $left = $mid + 1;
         }
     }
     
     return -1;
-
 }
 
 $array = range(1,100,2);
-
-var_dump( binary_search(43, $array) );
+$index = binary_search(43, $array);
+echo "index of {$array[$index]} is $index";
