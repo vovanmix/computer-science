cd7eb2013014e60531f588983587954fa675c981
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-13 19:59:54 -0700

    Create Heap.php

---
+++ DataStructures/Heap.php
@@ -0,0 +1,6 @@
+<?php
+
+# Heap is a binary tree, where all childs' keys should be less (or bigger) that their parents'.
+# Heaps are filled in order, top to bottom and left to right
+# When we add a new value, we compare it's key to it's parent's, and if it doesn't fit in the structure, 
+# we swap it with the parent, and then with it's parent
