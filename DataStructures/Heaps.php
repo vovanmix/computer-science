cd1406dd79ae9b6a5b1ec8448e66ecffbc1bb837
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-13 20:01:05 -0700

    Update and rename Heap.php to Heaps.php

--- DataStructures/Heaps.php
+++ DataStructures/Heaps.php
@@ -4,3 +4,5 @@
 # Heaps are filled in order, top to bottom and left to right
 # When we add a new value, we compare it's key to it's parent's, and if it doesn't fit in the structure, 
 # we swap it with the parent, and then with it's parent
+# Min heap: lowest key at the top, Max heap: biggest key at the top
+# Very useful for implementing Priority queues
