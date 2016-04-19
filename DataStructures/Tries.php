b2e7c20b04f9901342f90849a63a15120988fb1e
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-18 20:21:27 -0700

    Update Tries.php

--- DataStructures/Tries.php
+++ DataStructures/Tries.php
@@ -3,7 +3,9 @@
 # a tree where each node could have as much children as there are 
 # possible values in the datatype (a-z for letters, 0-9 for numbers etc)
 
-# add O(n) ??
+# very useful for searching in the dictionaries, to find if the sequence is a word or a prefix of a word
+
+# add O(1)
 # search "isPrefix" O(1)
 # search "contains" O(1)
 # Because the number of steps required to find a word is <= y
@@ -22,14 +24,40 @@ interface Vocabulary {
     
 }
 
-class Trie implements Vocabulary {
+class TrieNode {
     
-    public function __construct(){
-        
+    protected $is_a_word = false;
+    protected $children = [];
+    
+    public function __construct($is_a_word){
+        $this->is_a_word = $is_a_word;
     }
     
     public function add($word){
-        
+        $letter = substr($word, 0, 1);
+        $new_node = new TrieNode($is_a_word);
+        $this->children[$letter] = $new_node;
+        if(strlen($word) > 1){
+            $remaining_word = substr($word, 1);
+            $new_node->add($remaining_word);
+        }
+        else{
+            $this->is_a_word = true;
+        }
+    }
+    
+}
+
+class Trie implements Vocabulary {
+    
+    protected $head = null;
+    
+    public function __construct($is_a_word){
+        $this->head = new TrieNode(false);
+    }
+    
+    public function add($word){
+        $this->head->add($word);
     }
     
     public function isPrefix($prefix){
