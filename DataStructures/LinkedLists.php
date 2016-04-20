6e2394f3ae533d7db066d32797f0edcfd833946b
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-04-20 12:35:42 -0700

    Update LinkedLists.php

--- DataStructures/LinkedLists.php
+++ DataStructures/LinkedLists.php
@@ -4,6 +4,8 @@
 # and previous (double linked list) element. Elements can not be accessed by index. Elemets are accessed only in consequence.
 # Lists are better than arrays in adding elements (faster in memory). But is lists are implemented using arrays there is no difference.
 # Linear Search complexity: O(n) (arrays are better( O(log n)) in binary search)
+#
+# Animation: https://www.cs.usfca.edu/~galles/visualization/StackLL.html
 
 # 1. Built in implementstion
 $a = new SplDoublyLinkedList();
